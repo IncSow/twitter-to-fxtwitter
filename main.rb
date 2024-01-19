b16e4f587eafd71ba2191dbd44f05ae8742ede04
IncSow <lilo.incsow@gmail.com>
2024-01-19 22:33:47 +0100

    Linted, added a readme and extracted a bit of logic.

--- main.rb
+++ main.rb
@@ -1,32 +1,37 @@
+# frozen_string_literal: true
+
 require 'discordrb'
 require 'dotenv'
 Dotenv.load
 bot = Discordrb::Bot.new token: ENV['TOKEN']
 
-$twitter_link_regexp = /https:\/\/twitter\.com\/\S{1,}|https:\/\/x.com\/\S{1,}/i
+$twitter_link_regexp = %r{https://twitter\.com/\S{1,}|https://x.com/\S{1,}}i
 $twitter_domains_regexp = /x|twitter/i
 
-def fix_message_with_twitter(message, event)
-  matched = message.scan($twitter_link_regexp)
-  return unless matched.length&.positive?
+def format_match_list(matches)
+  "fxtwitter > twitter.\n#{matches.map do |match|
+                             match.gsub($twitter_domains_regexp, 'fxtwitter')
+                           end.join("\n")}"
+end
 
-  # deleter = matched.length > 1
+def fix_message_with_twitter(event)
+  message =  event.message.content
+  matches = message.scan($twitter_link_regexp)
+  return unless matches.length&.positive?
+
   deleter = false
+  # deleter = matches.length > 1
+
   if deleter
-    event.respond "<@#{event.message.author.id}> sent :\n#{message.gsub($twitter_domains_regexp, "fxtwitter")}"
+    event.respond "<@#{event.message.author.id}> sent :\n#{message.gsub($twitter_domains_regexp, 'fxtwitter')}"
     event.message.delete
   else
-    response = "fxtwitter > twitter.\n#{matched.map{|mes| mes.gsub($twitter_domains_regexp, 'fxtwitter')}.join("\n")}"
-    event.respond response
+    event.respond format_match_list(matches)
   end
 end
 
-bot.message() do |event|
-  message_content = event.message.content
-  fix_message_with_twitter(message_content, event)
+bot.message do |event|
+  fix_message_with_twitter(event)
 end
-
-
-
 
 bot.run
---
+++ readme.md
@@ -0,0 +1,29 @@
+# TWITTER LINKS TO FXTWITTER
+
+## Requirements
+This bot was coded using Ruby 3.1.2
+You will need to have your own discord bot token to run this bot.
+
+## Installing steps
+
+Once Ruby 3.1.2 is installed, simply run `bundle install`.
+Then, create a .env file containing your token, as such :
+`TOKEN=YOUR_TOKEN_HERE`
+
+You can then run this bot using `ruby main.rb`
+
+
+## More about this bot 
+Feel free to contribute, open new issues and PRs.
+This was done in an hour for fun, and may or may not evolve as time goes on.
+
+### TO-DO LIST :
+Link it to a database, so that each server can choose if they want their messages deleted or not. By default, no messages are deleted right now.
+To change this behavior in the meantime, simply uncomment the `deleted` variable.
+
+Add support for Instagram to ddInstagram !
+
+More to come . . .
+
+*IncSow*
+
